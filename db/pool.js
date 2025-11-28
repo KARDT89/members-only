600a126388a52f107a377f26e17819ed0ced1f5f
dt89 <tamalsarkartwoooo@gmail.com>
2025-11-28 14:58:19 +0000

    Added neondb

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,11 @@
+import { configDotenv } from "dotenv";
 import {Pool} from "pg";
+configDotenv()
 
 const pool = new Pool({
-    connectionString: "postgresql://dt89:pg123@localhost:5432/members_only"
+    // connectionString: "postgresql://dt89:pg123@localhost:5432/members_only"
+    connectionString: process.env.DATABASE_URL
+
 });
 
 export default pool;
